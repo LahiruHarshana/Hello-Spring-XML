ae00177306a28f7234bbdc71bcf5c60d3d3cdf36
LahiruHarshana <lharshana2002@gmail.com>
2024-02-02 15:14:51 +0530

    updated

--- src/main/java/lk/ijse/gdse66/spring/AppInitializer.java
+++ src/main/java/lk/ijse/gdse66/spring/AppInitializer.java
@@ -1,8 +1,9 @@
 package lk.ijse.gdse66.spring;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AppInitializer {
     public static void main(String[] args) {
-
-
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
     }
 }
